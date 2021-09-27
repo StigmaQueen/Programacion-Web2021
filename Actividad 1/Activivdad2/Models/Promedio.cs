f9bea07dd53d0f47e4ef4a4434b323a74b2a1c40
StigmaQueen <171g0218@rcarbonifera.tecnm.mx>
2021-09-27 00:44:49 -0500

    Actividad 1 y 2 de de programacion web

---
+++ Actividad 1/Activivdad2/Models/Promedio.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Activivdad2.Models
+{
+    public class Promedio
+    {
+        public int Cal1 { get; set; }
+        public int Cal2 { get; set; }
+        public int Cal3 { get; set; }
+        public int Resultado { get { return (Cal1 + Cal2 + Cal3) / 3; } }
+        
+    }
+}
